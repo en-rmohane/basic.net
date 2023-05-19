85d68a6f11268a6e718e205e8451775dab146ee6
“ravi <ravikumarmohane@gmail.com>
2023-05-19 23:07:58 +0530

    [ravi kumar]: adding attendance

---
+++ basic,net/basic,net/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class employee
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int attendance = random.Next(0, 2);
+
+        if (attendance == 0)
+        {
+            Console.WriteLine("employee is absent");
+        }
+        else
+        {
+            Console.WriteLine("employee is present");
+        }
+    }
+}
